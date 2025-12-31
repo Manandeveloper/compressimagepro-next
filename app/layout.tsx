3f903ed8f14ec9ec66dd0799e82fb53680a6c295
manandeveloper-wf <manandeveloper.wf@gmail.com>
2025-12-31 15:37:08 +0530

    last commit of year

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,8 @@
 // import "./custom-tailwind.css";
 import "./globals.css";
-import { Open_Sans } from 'next/font/google';
 import { Providers } from "./providers";
 import { MainLayout } from "@/components/layout/MainLayout";
-const openSans = Open_Sans({
-  subsets: ['latin'],
-  display: 'swap', // 'swap' ensures text is visible while the font loads
-  variable: '--font-open-sans', // Define a CSS variable name
-});
+
 export const metadata = {
   title: "Compress Image Pro",
   description: "Free online image, video and PDF tools",
